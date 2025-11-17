494aa56f65d80e9a0f94bb6ef86f798b799250a2
deadloct <53124227+deadloct@users.noreply.github.com>
2025-11-17 17:24:33 +1100

    Fix tests for recent updates

--- src/utils/utils.spec.js
+++ src/utils/utils.spec.js
@@ -5,33 +5,33 @@ import { calcIF } from './utils';
     {
         desc: "calc if: no bonuses",
         params: { rune1: 0, rune2: 0, guild: 0, consumable: 0, daily: 0, adgor: 0, encounter: 1, dailyMult: 1 },
-        result: 100,
+        result: "100.00",
     },
     {
         desc: "calc if: bitgor",
         params: { rune1: 0, rune2: 0, guild: 0, consumable: 500, daily: 0, adgor: 0, encounter: 1, dailyMult: 1 },
-        result: 600,
+        result: "600.00",
     },
     {
         desc: "calc if: bitgor on raids on Sunday with no IF runes",
         params: { rune1: 0, rune2: 0, guild: 13, consumable: 500, daily: 75, adgor: 50, encounter: 5, dailyMult: 1 },
-        result: 3690,
+        result: "3690.00",
     },
     {
         desc: "calc if: bitgor in wb heroic",
         params: { rune1: 0, rune2: 0, guild: 0, consumable: 500, daily: 0, adgor: 0, encounter: 1.5, dailyMult: 1 },
-        result: 900,
+        result: "900.00",
     },
     {
         desc: "calc if: max known bonuses",
         params: { rune1: 37.5, rune2: 37.5, guild: 13, consumable: 1200, daily: 75, adgor: 50, encounter: 5, dailyMult: 3 },
-        result: 8315,
+        result: "8315.00",
     },
     {
         // Same as prev but with 50% override
         desc: "calc if: sardinex override",
         params: { rune1: 37.5, rune2: 37.5, guild: 13, consumable: 1200, daily: 75, adgor: 50, encounter: 5, dailyMult: 50 },
-        result: 7440,
+        result: "7440.00",
     },
 ].forEach(t => {
     test(t.desc, () => {
